8a1292f4989c0787374f45901f49babdc694ad5b
saqibahmedkhan <129554182+saqibahmedkhan@users.noreply.github.com>
2023-12-28 19:05:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,19 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import pickle
+
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Number Input Example")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+number_input = st.number_input("Enter a number:", min_value=0.0, max_value=100.0, step=0.1)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.write("You entered:", number_input)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+with open('svm_model.pkl', 'rb') as model_file:
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    loaded_model = pickle.load(model_file)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+predictions = loaded_model.predict(X_test)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+accuracy = accuracy_score(y_test, predictions)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+print(f"Accuracy: {accuracy}")
